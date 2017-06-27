8184a29255959345efab3142d2d743bbfbb42955
Brook Kebede <brookabyss@gmail.com>
2017-06-27 14:13:20 -0700

    Adding to new repo

--- djangoEnv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/brookkebede/Desktop/Python_Assignments/python_stack/myEnvironments/djangoEnv/bin/python2.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
